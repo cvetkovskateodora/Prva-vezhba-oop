a1df04f33927e83c21501c9b9253a889ea3c870e
“Teodora <tivanovska@servsuite.onmicrosoft.com>
2022-11-09 22:31:19 +0100

    Prva zadacha

---
+++ helloWorld/HelloWorld2.java
@@ -0,0 +1,13 @@
+package helloWorld;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+        String pozdrav="  Hello World";
+        String fikt="OOP FIKT";
+        System.out.println("************************");
+	System.out.println(pozdrav+" "+fikt);
+        System.out.println("************************");
+    }
+
+}
